40b8a2752e39c2d866f6f0b0fec175922880e381
Rafi Andiansyah <rafiandiansyah01@gmail.com>
2025-11-25 12:21:22 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from './Card.jsx';
+
+function App() {
+    return (
+      <>
+      <Card /><br />
+      <Card /><br /><Card />  
+      
+      </>
+    );
+}
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://cdn.rafled.com/anime-icons/images/a49ba18f8665b168e2a9abe8a569e57ef70583e8941f766932ec243f88c30f23.jpg" alt="Profile picture" />
+            <h2>Ini Card Component</h2>
+            <p>Ini adalah paragraf dalam Card Component.</p>
+        </div>
+    );
+}
+
+export default Card;
