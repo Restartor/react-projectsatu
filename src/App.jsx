a5a9350117640424ff9b5a1d97c6a8d2af0e4348
Rafi Andiansyah <rafiandiansyah01@gmail.com>
2025-12-11 18:54:14 +0700

    membuat card with propsType

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import Card from './card/Card.jsx';
 import Button from './button/Button.jsx';
+import CardFilm from './Card Film/cardFilm.jsx';
+import styles from './Card Film/cardFilm.module.css';
 function App() {
     return (
       <>
       <Card />  <Card />
       <Button />
+      <div className={styles.cardFilmContainer}>
+        <CardFilm 
+          gambar="https://image.tmdb.org/t/p/original/hD4UoR4ZUXYBoKhKRwrJiTFLqCo.jpg"
+          judul="Tulsa King"
+          tahun="2025"
+          deskripsi="Tulsa king menceritakan tentang mantan komando mafia yang pensiun dan membuat kerajaan mafia baru"
+        />
+        <CardFilm 
+          gambar="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6vcDalR50RWa309vBH1NLmG2rjQ.jpg"
+          judul="John Wick 4"
+          tahun="2023"
+          deskripsi="John Wick kembali beraksi melawan musuh-musuh lamanya dalam pertempuran epik yang menegangkan."
+        />
+        <CardFilm 
+          gambar="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Y43POKjjKDGI9MH89NW0NAzzp8.jpg"
+          judul="Black Panther: Wakanda Forever"
+          tahun="2022"
+          deskripsi="Setelah kematian Raja T'Challa, Wakanda menghadapi ancaman baru dari musuh-musuh lama dan baru."
+        />
+        <CardFilm
+          judul="Avengers: Endgame"
+          tahun="2019"
+          deskripsi="Para pahlawan super bersatu untuk mengalahkan Thanos dan mengembalikan keseimbangan alam semesta."
+          gambar="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
+        />
+      </div>
+
       </>
     );
 }
+
 export default App
---
+++ src/Card Film/cardFilm.jsx
@@ -0,0 +1,31 @@
+import Card from '../card/Card';
+import PropTypes from 'prop-types';
+import styles from './cardFilm.module.css';
+
+function CardFilm(props){
+
+
+    return(        <div className={styles.cardFilm}>
+            <img src={props.gambar} alt="Poster Film" />
+            <h2>{props.judul}</h2>
+            <p className={styles.tahun}>{props.tahun}</p>
+            <p>{props.deskripsi}</p>
+        </div>
+    );
+}
+
+CardFilm.propTypes = {
+    gambar: PropTypes.string.isRequired,
+    judul: PropTypes.string.isRequired,
+    tahun: PropTypes.string.isRequired,
+    deskripsi: PropTypes.string.isRequired,
+}
+CardFilm.defaultProps = {
+    gambar: "https://www.shutterstock.com/image-photo/big-screen-rows-red-seats-600nw-2513489879.jpg",
+    judul: "Judul Film",
+    tahun: "Tahun Rilis",
+    deskripsi: "Deskripsi singkat tentang film ini.",
+}
+
+
+export default CardFilm;
